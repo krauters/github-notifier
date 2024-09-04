761fba2dfdec31b6583a0f11d10556cb7fd0273d
Colten Krauter <coltenkrauter@gmail.com>
2024-09-04 06:20:35 -0600

    feat: typescript-action to post PR data to Slack

---
+++ src/utils/github/structures.ts
@@ -0,0 +1,150 @@
+import type { context, GitHub } from '@actions/github/lib/utils.js'
+import type { OctokitOptions } from '@octokit/core'
+import type { GetResponseDataTypeFromEndpointMethod as GetType } from '@octokit/types'
+
+import { Octokit } from '@octokit/rest'
+
+export type Context = typeof context
+
+export type GitHubClient = InstanceType<typeof GitHub>
+
+const octokit = new Octokit()
+
+export type GitHubRepositories = GetType<typeof octokit.repos.listForOrg>
+export type GitHubRepository = GitHubRepositories[number]
+export type GitHubPulls = GetType<typeof octokit.pulls.list>
+export type GitHubPull = GitHubPulls[number]
+export type GitHubPullDetails = GetType<typeof octokit.pulls.get>
+export type GitHubPullCommits = GetType<typeof octokit.pulls.listCommits>
+export type GitHubPullRequestedReviewers = GetType<typeof octokit.pulls.listRequestedReviewers>
+export type GitHubPullComments = GetType<typeof octokit.issues.listComments>
+export type GitHubBranchRules = GetType<typeof octokit.repos.getBranchRules>
+export type GitHubBranchRule = GitHubBranchRules[number]
+export type GitHubPullReviews = GetType<typeof octokit.pulls.listReviews>
+export type GitHubUser = GetType<typeof octokit.users.getByUsername>
+export type GitHubOrgMemberships = GetType<typeof octokit.orgs.listMembershipsForAuthenticatedUser>
+export type GitHubPullReview = GitHubPullReviews[number]
+
+export enum GitHubReviewState {
+	Approved = 'APPROVED',
+	ChangesRequested = 'CHANGES_REQUESTED',
+	Commented = 'COMMENTED',
+	Dismissed = 'DISMISSED',
+	Pending = 'PENDING',
+}
+
+export type GitHubRepositoryType = 'all' | 'forks' | 'member' | 'private' | 'public' | 'sources' | undefined
+export enum RepositoryType {
+	All = 'all',
+	Forks = 'forks',
+	Member = 'member',
+	NonForks = 'sources',
+	Owner = 'owner',
+	Private = 'private',
+	Public = 'public',
+}
+
+export const reviewText: Record<string, string> = {
+	[GitHubReviewState.Approved]: 'approved this',
+	[GitHubReviewState.ChangesRequested]: 'requested changes',
+	[GitHubReviewState.Commented]: 'commented on this',
+}
+
+export interface Review {
+	context: string
+	email?: string
+	login: string
+	relativeHumanReadableAge: string
+	state: string
+	submittedAt: Date
+}
+export type Reviews = Record<string, Review>
+export interface Pull {
+	age: string
+	ageInHours: number
+	closedAt?: Date
+	commits?: number
+	createdAt: Date
+	createdBy: string
+	draft?: boolean
+	filesAndChanges?: FilesAndChanges
+	mergedAt?: Date
+	number: number
+	org: string
+	repo: string
+	repoUrl: string
+	requestedReviewers: string[]
+	reviewReport: ReviewReport
+	title: string
+	url: string
+	user?: GitHubUser
+}
+
+export enum PullState {
+	All = 'all',
+	Closed = 'closed',
+	Open = 'open',
+}
+
+export interface FilesAndChanges {
+	changes: number
+	files: number
+}
+
+export interface ReviewReport {
+	approvals?: number
+	approvalsRemaining?: number
+	changesRequested?: number
+	ghReviews: GitHubPullReviews
+	requiredReviewers?: number
+	reviews?: Reviews
+}
+
+export interface ReportItem {
+	minutesUntilFirstReview: number[]
+	minutesUntilMerged: number[]
+}
+
+export interface Organization {
+	data: GitHubOrgMemberships[number]
+	name: string
+}
+
+export interface ConfigurableGetPullsProps {
+	state?: PullState
+	withDrafts: boolean
+}
+
+export interface GetPullsProps extends ConfigurableGetPullsProps {
+	oldest?: Date
+	onlyGhReviews?: boolean
+	repositories: GitHubRepositories
+	withCommits?: boolean
+	withFilesAndChanges?: boolean
+	withUser?: boolean
+}
+
+export interface GetCommitsProps {
+	number: number
+	repo: string
+}
+
+export interface GitHubClientProps {
+	options?: OctokitOptions
+	token: string
+}
+
+export interface GetFilesAndChangesProps {
+	number: number
+	repo: string
+}
+
+export interface ConfigurableGetRepositoriesProps {
+	repositoryFilter: string[]
+	withArchived: boolean
+	withPublic: boolean
+}
+
+export interface GetRepositoriesProps extends ConfigurableGetRepositoriesProps {
+	type: GitHubRepositoryType
+}
---
+++ src/utils/slack/blocks.ts
@@ -0,0 +1,256 @@
+import type { KnownBlock, PlainTextElement, RichTextElement } from '@slack/web-api'
+
+import type { Pull } from '../github/structures.js'
+import type { SlackClient } from './slack-client.js'
+
+import { prBaseUrl, scmUrl } from '../../defaults.js'
+import { formatStringList, getAgeBasedEmoji, haveOrHas, plural } from '../misc.js'
+
+/**
+ * Get the first Slack block which includes a header and buttons.
+ * @param {string} org - The GitHub org.
+ * @param {string} header - The header text.
+ * @param {string} [text] - The sub-header text.
+ * @returns {KnownBlock[]}
+ */
+export function getFirstBlocks(org: string, header: string, text?: string): KnownBlock[] {
+	return [
+		{
+			text: {
+				emoji: true,
+				text: header,
+				type: 'plain_text',
+			},
+			type: 'header',
+		},
+		...(text ? getContextMarkdownBlock(text) : []),
+		{
+			elements: [
+				{
+					text: {
+						emoji: true,
+						text: `${org.toUpperCase()} Org`,
+						type: 'plain_text',
+					},
+					type: 'button',
+					url: `${scmUrl}/${org}`,
+				},
+				{
+					text: {
+						emoji: true,
+						text: `${org.toUpperCase()} PRs`,
+						type: 'plain_text',
+					},
+					type: 'button',
+					url: `${prBaseUrl}${org}`,
+				},
+			],
+			type: 'actions',
+		},
+	]
+}
+
+/**
+ * Get the last Slack block which includes footer markdown.
+ * @param {string} text - The footer text/markdown.
+ * @returns {KnownBlock[]}
+ */
+export function getLastBlocks(text: string): KnownBlock[] {
+	return getContextMarkdownBlock(text)
+}
+
+/**
+ * Get a context block with markdown text.
+ * @param {string} text - The footer text/markdown.
+ * @param {boolean} [withIndentation=false] - Include leading white space.
+ * @returns {KnownBlock[]}
+ */
+export function getContextMarkdownBlock(text: string, withIndentation = false): KnownBlock[] {
+	const elements: PlainTextElement[] = []
+	if (withIndentation) {
+		elements.push({
+			text: ' ',
+			type: 'plain_text',
+		})
+	}
+
+	return [
+		{
+			elements: [
+				...elements,
+				{
+					text,
+					type: 'mrkdwn',
+				},
+			],
+			type: 'context',
+		},
+	]
+}
+
+/**
+ * Get a pull block.
+ * @param {Pull} pull - Pull data.
+ * @param {SlackClient} slack - Slack client.
+ * @param {boolean} withUserMentions - Whether or not to mention Slack users.
+ * @returns {Promise<KnownBlock[]>}
+ */
+export async function getPullBlocks(pull: Pull, slack: SlackClient, withUserMentions: boolean): Promise<KnownBlock[]> {
+	const {
+		age,
+		ageInHours,
+		commits,
+		draft,
+		filesAndChanges,
+		number,
+		repo,
+		repoUrl,
+		requestedReviewers,
+		reviewReport,
+		title,
+		url,
+	} = pull
+	let ageBasedEmoji = getAgeBasedEmoji(ageInHours)
+	let approvedEmojiBlocks: (PlainTextElement | RichTextElement)[] = []
+	let draftEmojiBlocks: (PlainTextElement | RichTextElement)[] = []
+
+	if (draft) {
+		draftEmojiBlocks = getEmojiBlocks('pr-draft')
+	}
+
+	const { approvals, approvalsRemaining, requiredReviewers, reviews } = reviewReport
+
+	// Let's not give a green checkmark unless there is at least 1 approval
+	if (approvalsRemaining === 0 && approvals && approvals > 0) {
+		approvedEmojiBlocks = getEmojiBlocks('tl_certified_green')
+		ageBasedEmoji = ''
+	}
+
+	const context = [
+		`<${repoUrl}|${repo}>`,
+		`created ${age}${ageBasedEmoji}`,
+		commits && `${commits} ${plural('commit', commits)}`,
+		filesAndChanges && `${filesAndChanges.files} ${plural('file', filesAndChanges.files)}`,
+		filesAndChanges && `${filesAndChanges.changes} ${plural('change', filesAndChanges.changes)}`,
+		requiredReviewers && `${requiredReviewers} required ${plural('reviewer', requiredReviewers)}`,
+	].filter((item) => !!item)
+
+	const contextBlocks: KnownBlock[] = []
+	for (const review of Object.values(reviews ?? [])) {
+		const { context, email, login: username, relativeHumanReadableAge } = review
+		const slackUser = await slack.getSlackUser({
+			email,
+			username,
+		})
+
+		const displayName = slackUser?.profile?.display_name
+		const imageUrl = slackUser?.profile?.image_72
+
+		contextBlocks.push({
+			elements: [
+				{
+					text: ' ',
+					type: 'plain_text',
+				},
+				{
+					alt_text: 'Reviewer Slack avatar',
+					image_url: String(imageUrl),
+					type: 'image',
+				},
+				{
+					text: `*${displayName ?? username}* ${context} _${relativeHumanReadableAge}_.`,
+					type: 'mrkdwn',
+				},
+			],
+			type: 'context',
+		})
+	}
+
+	if (requestedReviewers.length) {
+		const slackUserIdsOrLogins: string[] = []
+		for (const username of requestedReviewers) {
+			const slackUser = await slack.getSlackUser({ username })
+			slackUserIdsOrLogins.push((withUserMentions && slackUser?.id && `<@${slackUser.id}>`) || username)
+		}
+		contextBlocks.unshift(
+			...getContextMarkdownBlock(
+				formatStringList(slackUserIdsOrLogins) +
+					` ${haveOrHas(slackUserIdsOrLogins.length)} been requested to review.`,
+				true,
+			),
+		)
+	}
+
+	return [
+		{
+			elements: [
+				{
+					elements: [
+						...(approvedEmojiBlocks as RichTextElement[]),
+						...(draftEmojiBlocks as RichTextElement[]),
+						{
+							style: {
+								bold: true,
+							},
+							text: `#${number} ${title}`,
+							type: 'link',
+							url,
+						},
+					],
+					type: 'rich_text_section',
+				},
+			],
+			type: 'rich_text',
+		},
+		{
+			elements: [
+				{
+					text: context.join('﹒'),
+					type: 'mrkdwn',
+				},
+			],
+			type: 'context',
+		},
+		...contextBlocks,
+		{
+			type: 'divider',
+		},
+	]
+}
+
+/**
+ * Get emoji blocks,.
+ * @param {string} name - The name of the emoji.
+ * @param {string} type - The type of section.
+ * @returns {(PlainTextElement | RichTextElement)[]}
+ */
+export function getEmojiBlocks(name?: string, type = 'rich_text_section'): (PlainTextElement | RichTextElement)[] {
+	if (!name) {
+		return []
+	}
+
+	if (type === 'rich_text_section') {
+		return [
+			{
+				name,
+				type: 'emoji',
+			},
+			{
+				text: ' ',
+				type: 'text',
+			},
+		]
+	}
+
+	if (type === 'context') {
+		return [
+			{
+				emoji: true,
+				text: `:${name}:`,
+				type: 'plain_text',
+			},
+		]
+	}
+
+	return []
+}
