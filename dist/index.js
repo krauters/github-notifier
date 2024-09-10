cdeef1d875e0666e7dd743098734aed0f3f2283b
Colten Krauter <18080897+coltenkrauter@users.noreply.github.com>
2024-09-09 21:36:19 -0600

    docs: support option (buy me a coffee)

--- dist/index.js
+++ dist/index.js
@@ -37661,7 +37661,7 @@ var core = __nccwpck_require__(2186);
 // EXTERNAL MODULE: ./node_modules/@actions/github/lib/github.js
 var github = __nccwpck_require__(5438);
 ;// CONCATENATED MODULE: ./package.json
-const package_namespaceObject = JSON.parse('{"u2":"@krauters/github-notifier","i8":"0.12.0","Xh":"https://github.com/krauters/github-notifier"}');
+const package_namespaceObject = JSON.parse('{"u2":"@krauters/github-notifier","i8":"0.13.0","Xh":"https://github.com/krauters/github-notifier"}');
 ;// CONCATENATED MODULE: ./src/defaults.ts
 const scmUrl = 'https://github.com';
 const prBaseUrl = `${scmUrl}/pulls?q=is%3Aopen+is%3Apr+archived%3Afalse+draft%3Afalse+user%3A`;
